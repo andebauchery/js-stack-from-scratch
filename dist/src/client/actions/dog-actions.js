755a33b705c8b4676f6087cd79e128bd6f1856b5
Andy Bauch <andy.bauch@gmail.com>
2016-12-27 16:16:15 -0800

    Add redux

---
+++ dist/src/client/actions/dog-actions.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var MAKE_BARK = exports.MAKE_BARK = 'MAKE_BARK';
+
+var makeBark = exports.makeBark = function makeBark() {
+  return {
+    type: MAKE_BARK,
+    payload: true
+  };
+};
+//# sourceMappingURL=dog-actions.js.map
---
+++ dist/src/client/components/message.jsx.js
@@ -0,0 +1,5 @@
+/**
+ * Created by andybauch on 12/27/16.
+ */
+"use strict";
+//# sourceMappingURL=message.jsx.js.map
